dff98ff4fc22c2dfbc8983e889d4c883bc1df36d
monicamun <mymunoznava@gmail.com>
2019-05-28 00:20:02 -0500

    Updates

---
+++ precache-manifest.73085cdf007c6b0b78273caaf48184c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab475a8e91fbdb6e03fcd200f811ad1f",
+    "url": "/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "938ecc267cbbbfe3e06f",
+    "url": "/GDL002-burger-queen/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "83516736df30d5c9144c",
+    "url": "/GDL002-burger-queen/static/css/main.6ca9ac3a.chunk.css"
+  },
+  {
+    "revision": "938ecc267cbbbfe3e06f",
+    "url": "/GDL002-burger-queen/static/js/2.356c2bac.chunk.js"
+  },
+  {
+    "revision": "83516736df30d5c9144c",
+    "url": "/GDL002-burger-queen/static/js/main.ac691b2e.chunk.js"
+  },
+  {
+    "revision": "f8c6079179fe30af807c",
+    "url": "/GDL002-burger-queen/static/js/runtime~main.2601196f.js"
+  }
+]);
