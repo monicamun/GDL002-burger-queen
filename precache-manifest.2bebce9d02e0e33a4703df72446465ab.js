fc5d6dc83e7eb6d1d9c7fb926d8038cc9bc729da
monicamun <mymunoznava@gmail.com>
2019-05-27 21:58:52 -0500

    Prepared for git hub pages.

---
+++ precache-manifest.2bebce9d02e0e33a4703df72446465ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88499b33ff7402ccb49d74a2abdf506",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb68a455a712c573bb90",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ed54082d11177dca9939",
+    "url": "/static/css/main.6ca9ac3a.chunk.css"
+  },
+  {
+    "revision": "eb68a455a712c573bb90",
+    "url": "/static/js/2.0191483d.chunk.js"
+  },
+  {
+    "revision": "ed54082d11177dca9939",
+    "url": "/static/js/main.aa632d7c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
